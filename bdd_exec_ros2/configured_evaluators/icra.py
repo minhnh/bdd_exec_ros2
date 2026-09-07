a899d7b9464c6f9ca9e130f7853b10b25c182a5a
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2026-09-07 14:22:17 +0200

    use same compliant limit for sim & real

--- bdd_exec_ros2/configured_evaluators/icra.py
+++ bdd_exec_ros2/configured_evaluators/icra.py
@@ -14,8 +14,8 @@ _ROBOT_WS = URIRef(f"{_SCENE}robot-ws")
 _TRAY_SIZE = (0.24, 0.20)
 _MARGIN_M = 0.05
 _ALLOWED_OUTSIDE_RATIO = 0.05
-_SIM_COMPLIANCE_LIMIT_N = 1.3 * 10.0
-_REAL_COMPLIANCE_LIMIT_N = 1.3 * 45.0
+_INTERACTION_DETECTION_THRESHOLD = 10.0  # N
+_COMPLIANCE_LIMIT_N = 1.5 * _INTERACTION_DETECTION_THRESHOLD
 
 wall_ws_center_inside = PlanarContainmentEvaluator(
     _DRAWER, _WALL_WS, (1.40, 0.646), margin_m=_MARGIN_M
@@ -40,21 +40,17 @@ robot_ws_footprint_inside = PlanarContainmentEvaluator(
     allowed_outside_ratio=_ALLOWED_OUTSIDE_RATIO,
 )
 
-sim_force_instantaneous_within_limit = WrenchForceNormWithinLimitEvaluator(
-    _SIM_COMPLIANCE_LIMIT_N
+force_instantaneous_within_limit = WrenchForceNormWithinLimitEvaluator(
+    _COMPLIANCE_LIMIT_N
 )
-sim_force_peak_within_limit = WrenchPeakForceNormWithinLimitEvaluator(
-    _SIM_COMPLIANCE_LIMIT_N
+force_peak_within_limit = WrenchPeakForceNormWithinLimitEvaluator(_COMPLIANCE_LIMIT_N)
+force_rms_within_limit = WrenchRmsForceNormWithinLimitEvaluator(_COMPLIANCE_LIMIT_N)
+estimated_force_instantaneous_within_limit = WrenchForceNormWithinLimitEvaluator(
+    _COMPLIANCE_LIMIT_N
 )
-sim_force_rms_within_limit = WrenchRmsForceNormWithinLimitEvaluator(
-    _SIM_COMPLIANCE_LIMIT_N
+estimated_force_peak_within_limit = WrenchPeakForceNormWithinLimitEvaluator(
+    _COMPLIANCE_LIMIT_N
 )
-real_force_instantaneous_within_limit = WrenchForceNormWithinLimitEvaluator(
-    _REAL_COMPLIANCE_LIMIT_N
-)
-real_force_peak_within_limit = WrenchPeakForceNormWithinLimitEvaluator(
-    _REAL_COMPLIANCE_LIMIT_N
-)
-real_force_rms_within_limit = WrenchRmsForceNormWithinLimitEvaluator(
-    _REAL_COMPLIANCE_LIMIT_N
+estimated_force_rms_within_limit = WrenchRmsForceNormWithinLimitEvaluator(
+    _COMPLIANCE_LIMIT_N
 )
